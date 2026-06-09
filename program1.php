a405d8d97b9e1d083537fc6378cd19e459ecea39
hetvipabari126099 <hetvi.pabari126099@marwadiuniversity.ac.in>
2026-06-09 10:32:27 +0530

    Add files via upload

---
+++ program1.php
@@ -0,0 +1,16 @@
+<?php
+$collegename='MARWADI UNIVERSITY';
+$studentname='HETVI PABARI';
+$enrollno=7108;
+$semester='sem 4';
+$marks=450;
+$totalmarks=500;
+$percentage=($marks/$totalmarks)*100;
+
+echo"<h2> PREVIOUS SEMESTER RESULT </h2>";
+echo "COLLEGE NAME:". $collegename ."<br>";
+echo "STUDENT NAME:". $studentname ."<br>";
+echo "ENROLL NO:". $enrollno ."<br>";
+echo "SEMESTER:". $semester ."<br>";
+echo "PERCENTAGE:". $percentage ."<br>";
+?>
---
+++ program2.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = 25;
+$num2 = 10;
+$num3 = 40;
+
+$max = max($num1, $num2, $num3);
+
+$min = min($num1, $num2, $num3);
+echo "numbers: $num1, $num2, $num3 <br>";
+echo "Maximum number = " . $max . "<br>";
+echo "Minimum number = " . $min;
+?>
